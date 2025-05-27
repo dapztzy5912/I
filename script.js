929988172d3bef8a8856f993699b774585eb57d4
dapztzy5912 <depannation999@gmail.com>
2025-05-27 12:35:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function sendData() {
-  const judul = encodeURIComponent(document.getElementById('judul').value);
-  const bahasa = encodeURIComponent(document.getElementById('bahasa').value);
-  const kode = encodeURIComponent(document.getElementById('kode').value);
+function submitCode() {
+  const judul = document.getElementById("judul").value;
+  const bahasa = document.getElementById("bahasa").value;
+  const kode = document.getElementById("kode").value;
 
-  const url = `hasil.html?judul=${judul}&bahasa=${bahasa}&kode=${kode}`;
-  window.location.href = url;
-  return false;
+  localStorage.setItem("judul", judul);
+  localStorage.setItem("bahasa", bahasa);
+  localStorage.setItem("kode", kode);
+
+  window.location.href = "hasil.html";
 }
